8e609bf917dd99f0e42934bb9906861608b3b81a
fgomezgf-sketch <fgomezgf@gmail.com>
2025-10-13 18:35:27 -0600

    Update Figure.java

--- figures/Figure.java
+++ figures/Figure.java
@@ -1,1 +1,35 @@
+package chess.figures;
+
+import chess.interfaces.IntFigure;
+
+
+public abstract class Figure implements IntFigure {
+    protected String color; 
+    protected char column;  
+    protected int row;     
+
+    public Figure() {}
+
+    public Figure(String color, char column, int row) {
+        this.color = color;
+        this.column = column;
+        this.row = row;
+    }
+
+    public String getColor() { return color; }
+    public char getColumn() { return column; }
+    public int getRow() { return row; }
+
+    public void setColumn(char column) { this.column = column; }
+    public void setRow(int row) { this.row = row; }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "{" +
+               "color='" + color + '\'' +
+               ", column=" + column +
+               ", row=" + row +
+               '}';
+    }
+}
 
