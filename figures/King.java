8422e4c13fa0ffaad9c9f7fe2e7493058134ec7a
SL4612 <sluevano612@gmail.com>
2025-10-14 23:34:26 -0600

    Update King.java

--- figures/King.java
+++ figures/King.java
@@ -1,1 +1,13 @@
+package figures;
+public class King extends Queen{
+  public King(String color, char posX, int posY){
+    super(color, posX, posY);
+  }
 
+  @Override
+  public boolean moveTo(char x, int y){
+    int dx = Math.abs(x - posX);
+    int dy = Math.abs(y - posY);
+    return dx <= 1 && dy <= 1;
+  }
+}
