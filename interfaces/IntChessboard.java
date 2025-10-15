fb0274c551f7b1db91594f05839a8af6d4b93523
fgomezgf-sketch <fgomezgf@gmail.com>
2025-10-15 13:01:03 -0600

    Update IntChessboard.java

--- interfaces/IntChessboard.java
+++ interfaces/IntChessboard.java
@@ -1,1 +1,13 @@
+package chess.interfaces;
+
+import chess.exceptions.InvalidCoordinateException;
+
+public interface IntChessBoard {
+    /**
+     * Verify coordinates are inside the chessboard.
+     * X is 1..8 (column index), Y is 1..8 (row).
+     * Throws InvalidCoordinateException if not valid.
+     */
+    boolean verifyCoordinate(int X, int Y) throws InvalidCoordinateException;
+}
 
