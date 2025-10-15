5e30c74b8beba741e92ce1e186a408f1e66e6e07
SL4612 <sluevano612@gmail.com>
2025-10-14 23:38:39 -0600

    Update Pawn.java

--- figures/Pawn.java
+++ figures/Pawn.java
@@ -1,1 +1,13 @@
+package figures;
+public class Pawn extends Figure{
+  public Pawn(String color, char posX, int posY){
+    super(color, posX, posY);
+  }
 
+  @Override
+  public boolean moveTo(char x, int y) throws Exception{
+    int dy = y - posY;
+    return(color.equalsIgnoreCase("white") && dy == 1) || (color.equalsIgnoreCase("black") && dy == -1);
+  }
+}
+    
