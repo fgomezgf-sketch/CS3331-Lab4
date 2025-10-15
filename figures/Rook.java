dc5b607c74c5b1a6014fd19958388dbdd77a6686
SL4612 <sluevano612@gmail.com>
2025-10-14 23:41:27 -0600

    Update Rook.java

--- figures/Rook.java
+++ figures/Rook.java
@@ -1,1 +1,11 @@
+package figures;
+public class Rook extends Figure{
+  public Rook(String color, char posX, int posY){
+    super(color, posX, posY);
+  }
 
+  @Override
+  public boolean moveTo(char x, int y){
+    return(x == posX || y == posY);
+  }
+}
