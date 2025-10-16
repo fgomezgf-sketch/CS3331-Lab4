58de32131d5ed943b8141d6901c279eb6f46b462
SL4612 <sluevano612@gmail.com>
2025-10-16 04:07:32 -0600

    Update Queen.java

--- figures/Queen.java
+++ figures/Queen.java
@@ -1,1 +1,17 @@
+package figures;
+import interfaces.IntBishop;
+import exceptions.InvalidMoveException;
 
+public class Queen extends Rook implements IntBishop{
+  public Queen(String color, char posX, int posY){
+    super(color, posX, posY);
+  }
+
+  @Override
+  public Boolean moveToBishop(char x, int y) throws InvalidMoveException{
+    if(Math.abs(x - posX) == Math.abs(y - posY)) return true;
+    return false;
+  }
+
+  @Override
+  public 
