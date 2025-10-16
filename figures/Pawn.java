bc629ce583bc01b65c60a821b55487e21df52de3
SL4612 <sluevano612@gmail.com>
2025-10-16 02:38:07 -0600

    Update Pawn.java

--- figures/Pawn.java
+++ figures/Pawn.java
@@ -5,9 +5,10 @@ public class Pawn extends Figure{
   }
 
   @Override
-  public boolean moveTo(char x, int y) throws Exception{
+  public boolean moveTo(char x, int y) throws InvalidMoveException{
     int dy = y - posY;
     return(color.equalsIgnoreCase("white") && dy == 1) || (color.equalsIgnoreCase("black") && dy == -1);
+    throw new InvalidMoveException("Invalid move for Pawn from " + posX + posY + " to " + x + y);
   }
 }
     
