c4b16f4543db9423bcdc55f5414827dcae5be66d
SL4612 <sluevano612@gmail.com>
2025-10-16 02:35:48 -0600

    Update King.java

--- figures/King.java
+++ figures/King.java
@@ -1,13 +1,17 @@
 package figures;
+
+import exceptions.InvalidMoveException;
+
 public class King extends Queen{
   public King(String color, char posX, int posY){
     super(color, posX, posY);
   }
 
   @Override
-  public boolean moveTo(char x, int y){
+  public boolean moveTo(char x, int y) throws InvalidMoveException{
     int dx = Math.abs(x - posX);
     int dy = Math.abs(y - posY);
     return dx <= 1 && dy <= 1;
+    throw new InvalidMoveException("Invalid move for King from " + posX + posY + " to " + x + y);
   }
 }
